9b349f5b22f590abbfaf8bd96cfafae1ab2aee0f
shadowjilu <shadowjilu@gmail.com>
2017-03-08 11:16:25 +0800

    bug

--- Happiness/HappinessViewController.swift
+++ Happiness/HappinessViewController.swift
@@ -23,7 +23,21 @@ class HappinessViewController: UIViewController, FaceViewDataSource
         static let HappinessGestureScale: CGFloat = 4
     }
     
-    func changeHappiness(gesture: UIPanGestureRecognizer)  {
+//    func changeHappiness(gesture: UIPanGestureRecognizer)  {
+//        switch gesture.state {
+//        case .ended: fallthrough
+//        case .changed:
+//            let translation = gesture.translation(in: faceView)
+//            let happinessChange = -Int(translation.y / Constant.HappinessGestureScale)
+//            if happinessChange != 0 {
+//                happiness += happinessChange
+//                gesture.setTranslation(CGPoint.zero, in: faceView)
+//            }
+//        default: break
+//        }
+//    }
+
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .ended: fallthrough
         case .changed:
@@ -35,15 +49,15 @@ class HappinessViewController: UIViewController, FaceViewDataSource
             }
         default: break
         }
-    }
 
+    }
     
     @IBOutlet weak var faceView: FaceView!{
         didSet{
             faceView.dataSouce = self
             faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(FaceView.scale(gesture:))))
             //为什么这里target用faceView，为什么scale不能写在controller里面然后self.cale
-            faceView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.changeHappiness(gesture:))))
+//            faceView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(self.changeHappiness(gesture:))))
             //为什么这里target用self
         }
     
